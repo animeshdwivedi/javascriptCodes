6fb6aaa25d2264a4bec795984faf873a15122f4f
Animesh Dwivedi <animeshdwivedinwm@gmail.com>
2020-06-14 20:17:16 -0500

    Adding changes to test ES6

--- BasicJSCode/HelloWorld.test.js
+++ BasicJSCode/HelloWorld.test.js
@@ -1,7 +1,10 @@
-import { helloWorld } from './HelloWorld';
+import { helloWorldES6, helloWorld } from './HelloWorld';
 
 describe('Hello World', () => {
-  it('Should Print Hello World', () => {
+  it('Should Print Hello World for ES5 function', () => {
     expect(helloWorld()).toBe('Hello World');
+  });
+  it('Should Print Hello World for ES6 function', () => {
+    expect(helloWorldES6('Hello World')).toEqual('Hello World');
   });
 });
